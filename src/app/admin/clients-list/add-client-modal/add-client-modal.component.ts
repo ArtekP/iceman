ad7b3ac432e2470ca6193c0f70ec67cb18c9a2e3
abc <abc@abc.abc>
2022-05-09 20:51:30 +0200

    add: added custom email validation instead of angular one

--- src/app/admin/clients-list/add-client-modal/add-client-modal.component.ts
+++ src/app/admin/clients-list/add-client-modal/add-client-modal.component.ts
@@ -12,7 +12,7 @@ import { AuthService } from 'src/app/auth/auth.service';
 export class AddClientModalComponent {
   public form = new FormGroup({
     name: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.required, Validators.email]),
+    email: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
     password: new FormControl('', [Validators.required, Validators.minLength(6),])
   })
 
