2a989674ce729c3da05f61ea07961baa3bd38a2f
Artur Pożyczka <artur.pozyczka@ppuc.pl>
2022-05-11 11:19:21 +0200

    fix: cant multiclick login btn

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -18,6 +18,8 @@ import {
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  public isLoginBtnClicked = false;
+
   public form = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
     password: new FormControl('', [Validators.required, Validators.minLength(6)])
@@ -42,6 +44,7 @@ export class LoginComponent {
   }
 
   public onSubmit() {
+    this.isLoginBtnClicked = true;
     this.authService.signIn(this.form.controls['email'].value, this.form.controls['password'].value);
   }
 }
