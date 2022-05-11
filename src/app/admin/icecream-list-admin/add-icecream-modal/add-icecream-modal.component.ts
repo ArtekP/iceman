ab620c4bc117a58795d47206961392414564b237
Artur Pożyczka <artur.pozyczka@ppuc.pl>
2022-05-11 12:12:04 +0200

    fix: validation fix

--- src/app/admin/icecream-list-admin/add-icecream-modal/add-icecream-modal.component.ts
+++ src/app/admin/icecream-list-admin/add-icecream-modal/add-icecream-modal.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { IcecreamService } from '../../../user/icecream-list/icecream.service';
 
 @Component({
@@ -13,9 +14,13 @@ export class AddIcecreamModalComponent {
     name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
   })
 
-  constructor(private icecreamService: IcecreamService) { }
+  constructor(private icecreamService: IcecreamService, private toast: ToastrService) { }
 
   public onSubmit(name: string) {
-    this.icecreamService.addIcecream(name);
+    if(/^[a-z0-9]+$/i.test(name)) {
+      this.icecreamService.addIcecream(name);
+    } else {
+      this.toast.error('Nazwa może składać się tylko z liter i cyfr!')
+    }
   }
 }
